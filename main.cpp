2a0a90681e4a183660ffb6b4c363961370dfa291
Valentina Varela <vavarela.rodriguez@gmail.com>
2020-08-02 22:45:47 -0300

    feature: Add different size for rows and cols

--- main.cpp
+++ main.cpp
@@ -6,40 +6,40 @@ int main() {
     Matrix<char>* letters = new Matrix<char>(10, '-');
     char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    for (int i = 0; i < letters->getSize(); i++) {
-        for (int j = 0; j < letters->getSize(); ++j) {
+    for (int i = 0; i < letters->getRows(); i++) {
+        for (int j = 0; j < letters->getCols(); ++j) {
             letters->insert(alphabet[i], i, j);
         }
     }
     letters->printMatrix();
-    letters->resize(5);
+    letters->resize(5,5);
     letters->printMatrix();
-    letters->resize(15);
+    letters->resize(15,15);
     letters->printMatrix();
-    for (int i = 0; i < letters->getSize(); i++) {
-        for (int j = 0; j < letters->getSize(); j++) {
+    for (int i = 0; i < letters->getRows(); i++) {
+        for (int j = 0; j < letters->getCols(); j++) {
             letters->insert(alphabet[i], i, j);
         }
     }
     letters->printMatrix();
-    letters->resize(25);
+    letters->resize(25, 25);
     letters->printMatrix();
 
 
     cout << "\n\t\t---------------------------- NUMBERS -----------------------------\n";
     Matrix<int>* numbers = new Matrix<int>(15, 0);
-    for (int i = 0; i < numbers->getSize(); i++) {
-        for (int j = 0; j < numbers->getSize(); j++) {
+    for (int i = 0; i < numbers->getRows(); i++) {
+        for (int j = 0; j < numbers->getCols(); j++) {
             numbers->insert(i, j, i);
         }
     }
     numbers->printMatrix();
-    numbers->resize(2);
+    numbers->resize(2, 2);
     numbers->printMatrix();
-    numbers->resize(10);
+    numbers->resize(10, 10);
     numbers->printMatrix();
-    for (int i = 0; i < numbers->getSize(); i++) {
-        for (int j = 0; j < numbers->getSize(); ++j) {
+    for (int i = 0; i < numbers->getRows(); i++) {
+        for (int j = 0; j < numbers->getCols(); ++j) {
             numbers->insert(1, i, j);
             numbers->insert(0, j, i);
         }
